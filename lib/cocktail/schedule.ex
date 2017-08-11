08cc7a88e703880976e8344572584be39bbbe85b
Chris Dosé <chris.dose@gmail.com>
2017-08-11 15:53:23 -0700

    Simplify schedule module and fix start_time bug.

--- lib/cocktail/schedule.ex
+++ lib/cocktail/schedule.ex
@@ -7,21 +7,21 @@ defmodule Cocktail.Schedule do
     %__MODULE__{ start_time: start_time }
   end
 
-  def add_recurrence_rule(%__MODULE__{ recurrence_rules: recurrence_rules } = schedule, :daily) do
-    %{ schedule | recurrence_rules: recurrence_rules ++ [Rules.daily] }
+  def add_recurrence_rule(%__MODULE__{} = schedule, :daily) do
+    %{ schedule | recurrence_rules: schedule.recurrence_rules ++ [Rules.daily] }
   end
 
-  def occurrences(%__MODULE__{ start_time: schedule_start_time } = schedule, start_time \\ nil) do
-    start_time = start_time || schedule_start_time
-    Stream.unfold({ schedule, start_time, start_time }, &next_time/1)
+  def occurrences(%__MODULE__{} = schedule, start_time \\ nil) do
+    start_time = start_time || schedule.start_time
+    Stream.unfold({ schedule, start_time }, &next_time/1)
   end
 
-  def next_time({ %__MODULE__{ recurrence_rules: recurrence_rules } = schedule, start_time, time }) do
+  def next_time({ %__MODULE__{} = schedule, time }) do
     time =
-      recurrence_rules
-      |> Enum.map(&Rules.next_time(&1, start_time, time))
+      schedule.recurrence_rules
+      |> Enum.map(&Rules.next_time(&1, schedule.start_time, time))
       |> Enum.min
 
-    { time, { schedule, start_time, Timex.shift(time, seconds: 1) } }
+    { time, { schedule, Timex.shift(time, seconds: 1) } }
   end
 end
