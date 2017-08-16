f9e5f2507b926056130991c25d723007333952c4
Chris Dosé <chris.dose@gmail.com>
2017-08-16 14:35:57 -0700

    Comparing spans for sorting

--- lib/cocktail/span.ex
+++ lib/cocktail/span.ex
@@ -2,4 +2,7 @@ defmodule Cocktail.Span do
   defstruct [:from, :until]
 
   def new(from, until), do: %__MODULE__{from: from, until: until}
+
+  def compare(%__MODULE__{from: t, until: until1}, %__MODULE__{from: t, until: until2}), do: until1 <= until2
+  def compare(%__MODULE__{from: from1}, %__MODULE__{from: from2}), do: from1 <= from2
 end
