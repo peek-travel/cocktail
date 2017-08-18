ca6dec28393f530bcab9f416fa71f52274dca626
Chris Dosé <chris.dose@gmail.com>
2017-08-18 14:40:08 -0700

    Add span overlap mode functions

--- lib/cocktail/span.ex
+++ lib/cocktail/span.ex
@@ -5,4 +5,12 @@ defmodule Cocktail.Span do
 
   def compare(%__MODULE__{from: t, until: until1}, %__MODULE__{from: t, until: until2}), do: until1 <= until2
   def compare(%__MODULE__{from: from1}, %__MODULE__{from: from2}), do: from1 <= from2
+
+  def overlap_mode(%__MODULE__{from: from, until: until}, %__MODULE__{from: from, until: until}), do: :is_equal_to
+  def overlap_mode(%__MODULE__{from: from1, until: until1}, %__MODULE__{from: from2, until: until2}) when from1 <= from2 and until1 >= until2, do: :contains
+  def overlap_mode(%__MODULE__{from: from1, until: until1}, %__MODULE__{from: from2, until: until2}) when from1 >= from2 and until1 <= until2, do: :is_inside
+  def overlap_mode(%__MODULE__{until: until1}, %__MODULE__{from: from2}) when until1 <= from2, do: :is_before
+  def overlap_mode(%__MODULE__{from: from1}, %__MODULE__{until: until2}) when from1 >= until2, do: :is_after
+  def overlap_mode(%__MODULE__{from: from1, until: until1}, %__MODULE__{from: from2, until: until2}) when from1 < from2 and until1 < until2, do: :overlaps_the_start_of
+  def overlap_mode(%__MODULE__{from: from1, until: until1}, %__MODULE__{from: from2, until: until2}) when from1 > from2 and until1 > until2, do: :overlaps_the_end_of
 end
