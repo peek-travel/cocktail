3ddb0a72864c831f0a40570f13f6d054870f8492
Chris Dosé <chris.dose@gmail.com>
2017-08-21 17:24:32 -0700

    Fix compare and overlap mode

--- lib/cocktail/span.ex
+++ lib/cocktail/span.ex
@@ -3,14 +3,24 @@ defmodule Cocktail.Span do
 
   def new(from, until), do: %__MODULE__{from: from, until: until}
 
-  def compare(%__MODULE__{from: t, until: until1}, %__MODULE__{from: t, until: until2}), do: until1 <= until2
-  def compare(%__MODULE__{from: from1}, %__MODULE__{from: from2}), do: from1 <= from2
+  def compare(%__MODULE__{from: t, until: until1}, %__MODULE__{from: t, until: until2}), do: Timex.compare(until1, until2) <= 0
+  def compare(%__MODULE__{from: from1}, %__MODULE__{from: from2}), do: Timex.compare(from1, from2) <= 0
 
   def overlap_mode(%__MODULE__{from: from, until: until}, %__MODULE__{from: from, until: until}), do: :is_equal_to
-  def overlap_mode(%__MODULE__{from: from1, until: until1}, %__MODULE__{from: from2, until: until2}) when from1 <= from2 and until1 >= until2, do: :contains
-  def overlap_mode(%__MODULE__{from: from1, until: until1}, %__MODULE__{from: from2, until: until2}) when from1 >= from2 and until1 <= until2, do: :is_inside
-  def overlap_mode(%__MODULE__{until: until1}, %__MODULE__{from: from2}) when until1 <= from2, do: :is_before
-  def overlap_mode(%__MODULE__{from: from1}, %__MODULE__{until: until2}) when from1 >= until2, do: :is_after
-  def overlap_mode(%__MODULE__{from: from1, until: until1}, %__MODULE__{from: from2, until: until2}) when from1 < from2 and until1 < until2, do: :overlaps_the_start_of
-  def overlap_mode(%__MODULE__{from: from1, until: until1}, %__MODULE__{from: from2, until: until2}) when from1 > from2 and until1 > until2, do: :overlaps_the_end_of
+  def overlap_mode(%__MODULE__{from: from1, until: until1}, %__MODULE__{from: from2, until: until2}) do
+    cond do
+      Timex.compare(from1, from2) <= 0 && Timex.compare(until1, until2) >= 0 ->
+        :contains
+      Timex.compare(from1, from2) >= 0 && Timex.compare(until1, until2) <= 0 ->
+        :is_inside
+      Timex.compare(until1, from2) <= 0 ->
+        :is_before
+      Timex.compare(from1, until2) >= 0 ->
+        :is_after
+      Timex.compare(from1, from2) < 0 && Timex.compare(until1, until2) < 0 ->
+        :overlaps_the_start_of
+      Timex.compare(from1, from2) > 0 && Timex.compare(until1, until2) > 0 ->
+        :overlaps_the_end_of
+    end
+  end
 end
