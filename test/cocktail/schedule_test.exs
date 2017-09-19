63b40a0611743cde7f8e726bd999a8b08fb951b9
Chris Dosé <chris.dose@gmail.com>
2017-09-18 22:21:20 -0700

    Add test for end_all_recurrence_rules

--- test/cocktail/schedule_test.exs
+++ test/cocktail/schedule_test.exs
@@ -1,5 +1,30 @@
 defmodule Cocktail.ScheduleTest do
   use ExUnit.Case
 
+  alias Cocktail.{Schedule, Rule}
+
   doctest Cocktail.Schedule, import: true
+
+  test "ending all recurrence rules of a schedule" do
+    schedule =
+      ~N[2017-09-09 09:00:00]
+      |> Schedule.new
+      |> Schedule.add_recurrence_rule(:daily, interval: 2)
+      |> Schedule.add_recurrence_rule(:daily, interval: 3)
+      |> Schedule.add_recurrence_rule(:daily, interval: 5)
+      |> Schedule.end_all_recurrence_rules(~N[2017-10-09 09:00:00])
+
+    assert %Schedule{recurrence_rules: rules} = schedule
+
+    Enum.each(rules, fn(%Rule{until: until}) ->
+      assert until == ~N[2017-10-09 09:00:00]
+    end)
+
+    times =
+      schedule
+      |> Schedule.occurrences
+      |> Enum.to_list
+
+    assert length(times) == 23
+  end
 end
